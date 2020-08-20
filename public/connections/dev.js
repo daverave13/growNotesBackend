693992f58a754c4df20f17cd636ee5755184746e
David Slusser <dslusser13@gmail.com>
2020-08-19 23:20:15 -0700

    updated db reference to heroku hosted db

--- public/connections/dev.js
+++ public/connections/dev.js
@@ -1,8 +1,11 @@
 const conDev = {
-  host: "localhost",
-  user: "dave",
-  password: "redred",
-  database: "prod"
+  host: "us-cdbr-east-02.cleardb.com",
+  user: "bf5ba5b3233022",
+  password: "a28136d6",
+  database: "heroku_ca9353bbce3ea2a?"
 };
 
 module.exports = conDev;
+
+
+//mysql://bf5ba5b3233022:a28136d6@us-cdbr-east-02.cleardb.com/heroku_ca9353bbce3ea2a?reconnect=true
